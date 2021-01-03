d4ba39359b35ffd4c12f59f5c7c8168a54f3b93b
JaPut <50654418+JaPut@users.noreply.github.com>
2021-01-03 14:12:05 +0200

    2021start

---
+++ content/que1.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Kas notiek</title>
+    <meta content="" name="JP">
+    <meta content="" name="mindofspace">
+    <meta content="" name="free">
+    <link href="img/JP.JPG" rel="icon">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <link href="../lib/bootstrap/css/bootstrap.min.css" rel="stylesheet"> 
+    <link href="../css/questyle.css" rel="stylesheet">
+    <link href="../lib/bootstrap/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+</head>
+<body>
+    <div class="fixed">
+        <a href="../index1.html"><i class="fa fa-home fa-lg" style="font-size:36px"></i></a></div>
+    <div class="header">
+        <p class="spin3">Kas notiek</p>
+        <!-- Kas notiek -->
+    </div>
+    <div class="mainmessage">
+        <p class="spin2">Kas notiek</p>
+    </div>
+    <div class="mainmessage">
+        <p class="spin2">Kas notiek</p>
+    </div>
+</div>
+</body>
+</html>
--- js/main.js
+++ js/main.js
@@ -1,5 +1,64 @@
 jQuery(document).ready(function($) {
  
+  //Check to see if the window is top if not then display button
+ $(window).scroll(function(){
+
+  // Show button after 500px
+  var showAfter = 500;
+  if ( $(this).scrollTop() > showAfter ) { 
+   $('#myBtn').fadeIn();
+  } else { 
+   $('#myBtn').fadeOut();
+  }
+ });
  
+ //Click event to scroll to top
+ $('#myBtn').click(function(){
+  $('html, body').animate({scrollTop : 0},800);
+  return false;
+ });
+
+ //show/hide about
+ $("#ka").click(function(){
+  $(".hide1").show(800);
+  $(".hide2, .hide3, .hide4").hide();
+});
+ $("#kapec").click(function(){
+  $(".hide1, .hide3, .hide4").hide();
+  $(".hide2").show(800);
+});
+ $("#kadaveida").click(function(){
+  $(".hide1, .hide2, .hide4").hide();
+  $(".hide3").show(800);
+});
+ $("#kas").click(function(){
+  $(".hide1, .hide2, .hide3").hide();
+  $(".hide4").show(800);
+});
+
+// show hide fakti
+$('#aks').click(function(){
+$('#fakti').show(1800);
+});
+$('#aizv, #aizvsp').click(function(){
+$('#fakti').hide(500);
+$('#sponk').hide(500);
+$('#spong').hide(500);
+});
+
+// show hide fakti
+$('#spon1').click(function(){
+$('#spong').show(500);
+$('#sponk').hide(500);
+});
+$('#spon2').click(function(){
+$('#sponk').show(500);
+$('#spong').hide(500);
+});
 
 });
+
+
+
+
+
